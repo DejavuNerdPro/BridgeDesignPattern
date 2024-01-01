71312b0fa02f9bcfff94944697ae0cccb4f86d48
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2024-01-02 02:31:53 +0630

    Pizza Implementation

---
+++ Implementation/VeggiePizza.java
@@ -0,0 +1,17 @@
+package Implementation;
+
+public class VeggiePizza extends Pizza {
+
+    @Override
+    public void assemble() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'assemble'");
+    }
+
+    @Override
+    public void qualityCheck() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'qualityCheck'");
+    }
+
+}
