ddd8ed5262a9a61eb8ac516f3098e2e9ee0f3b7c
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2024-01-02 02:39:44 +0630

    ItelianRestaurent Implement

--- Abstraction/ItelianRestaurent.java
+++ Abstraction/ItelianRestaurent.java
@@ -1,5 +1,26 @@
 package Abstraction;
 
-public class ItelianRestaurent {
+import Implementation.Pizza;
+
+public class ItelianRestaurent extends Restaurent {
+
+    public ItelianRestaurent(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public void addToppings() {
+        pizza.setToppings("-");
+    }
+
+    @Override
+    public void addSauce() {
+        pizza.setSauce("Oil");
+    }
+
+    @Override
+    public void makeCrust() {
+        pizza.setCrust("Thin");
+    }
 
 }
